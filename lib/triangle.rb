c525cc75b89b1c33e64344a9dc1f807e419db983
Yakura Marzett <marzettyakura@yahoo.com>
2020-04-17 23:54:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,35 +1,41 @@
 class Triangle
-  attr_accessor :x, :y, :z
+  attr_accessor :side1, :side2, :side3
   @sides = []
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
-    @sides = [x,y,z]
+  def initialize(side1, side2, side3)
+    @side1, @side2, @side3 = side1, side2, side3
+    @sides = [side1, side2, side3]
   end
-
   def kind
-    if kosher_triangle == true && kosher_triangle2 == true
-      puts true
-    else
+    if valid? == false
       raise TriangleError
-    end
-
-    if x == y && y == z && x == z
+    elsif(equilateral?)
       :equilateral
-    elsif x != y && y != z && z != x
+    elsif(scalene?)
       :scalene
-    else
+    elsif(isosceles?)
       :isosceles
     end
   end
-
-  def kosher_triangle
-    x.positive? && y.positive? && z.positive?
+  def equilateral?
+    (@side1 == @side2) && (@side1 == @side3)
   end
-
-  def kosher_triangle2
-    x + y > z && x + z > y && y + z > x
+  def scalene?
+    (@side1 != @side2) && (@side1!= @side3) && (@side2 != @side3)
+  end
+  def isosceles?
+    (@side2 == @side3) || (@side1 == @side2) || (@side1 == @side3)
+  end
+  def valid?
+    if (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
+      return true
+    else
+      raise TriangleError
+    end
+  end
+  class TriangleError < StandardError
+    def message
+      "This triangle is not valid."
+    end
   end
 end
 
